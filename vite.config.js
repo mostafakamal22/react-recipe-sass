0c033e456356e5922121786558276b39617e7a5f
mostafakamal22 <mk1319@fayoum.edu.eg>
2025-01-28 21:53:18 +0200

    update the router config#2

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { defineConfig } from "vite";
 import { fileURLToPath } from "url";
 
 // https://vite.dev/config/
