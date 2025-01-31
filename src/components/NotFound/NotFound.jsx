0165a2f48ba90cf5e3b3449aa19bfe9154d238e2
mostafakamal22 <mk1319@fayoum.edu.eg>
2025-02-01 00:44:56 +0200

    update not found component layout

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,3 +1,15 @@
+import { HiHome } from "react-icons/hi";
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div>
+      <p className="errorMessage">
+        Page not found. Please go back to the home page.
+      </p>
+      <Link to="/" className="homeButton">
+        <HiHome /> Go Home
+      </Link>
+    </div>
+  );
 }
