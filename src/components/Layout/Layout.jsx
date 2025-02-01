5e7b9a247acb0afde54a9c0d125c88cb3d8ff64c
mostafakamal22 <mk1319@fayoum.edu.eg>
2025-02-02 00:34:16 +0200

    add error boundary and fallback to the app

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
 import Footer from "../Footer/Footer";
 import Sidebar from "../Sidebar/Sidebar";
 import Main from "../Main/Main";
 import SidebarToggle from "../SidebarToggle/SidebarToggle";
+import BoundaryError from "../Shared/BoundaryError";
 
 export default function Layout() {
   return (
@@ -12,7 +14,9 @@ export default function Layout() {
         <Sidebar />
 
         <Main>
-          <Outlet />
+          <ErrorBoundary fallback={<BoundaryError />}>
+            <Outlet />
+          </ErrorBoundary>
         </Main>
       </div>
       <Footer />
---
+++ src/components/Shared/BoundaryError.jsx
@@ -0,0 +1,9 @@
+export default function BoundaryError() {
+  return (
+    <div>
+      <p className="errorMessage">
+        Something went wrong. Please refresh the page or try again later.
+      </p>
+    </div>
+  );
+}
